b9d967b381c2deb306eee4f4ca905032799c64f5
Michalis Fournarakis <mixfoudev@gmail.com>
2023-04-07 17:24:25 +0300

    exercise completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-exercise-0';
+  formInput = '';
+  odds: string[] = [];
+  evens: string[] = [];
+
+  /* Δεν χρειαζόταν να έχουμε δύο διαφορετικά components για τις λέξεις. */
+  /* Θα μπορούσαμε να χρησιμοποιήσουμε ένα και για τις 2 περιπτώσεις. */
+
+  onSubmit() {
+    if (this.formInput === '') return;
+
+    if (this.isEven(this.formInput)) this.evens.push(this.formInput);
+    else this.odds.push(this.formInput);
+
+    this.formInput = '';
+  }
+
+  isEven(word: string) {
+    return word.length % 2 == 0;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { OddLengthWordsComponent } from './odd-length-words/odd-length-words.component';
+import { EvenLengthWordsComponent } from './even-length-words/even-length-words.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserInputComponent,
+    OddLengthWordsComponent,
+    EvenLengthWordsComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
